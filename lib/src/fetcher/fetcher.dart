f0f5061533ff1ccd003b66b47542b74e79207d3e
Alisher Narzullayev <alishernarzullayev7777@gmail.com>
2025-05-31 15:19:09 +0500

    feat: add `onStatusChange` callback to `Fetcher.fetchWithBase`
    This commit introduces an optional `onStatusChange` callback to the `Fetcher.fetchWithBase` method. This callback allows consumers to react to changes in the `BaseStatus` (loading, success, error, initial) during the fetching process.
    
    Key changes:
    - Added `onStatusChange` parameter to `Fetcher.fetchWithBase`.
    - Invoked `onStatusChange` at different stages of the fetching lifecycle.
    - Updated `cubit_base.dart` to export the `Fetcher` class.
    - Added a new test case in `cubit_base_test.dart` to verify the functionality of `onStatusChange` and `Fetcher.fetchWithBase`.
    - Removed the `Calculator` class and its associated test, as it was placeholder code.

--- lib/src/fetcher/fetcher.dart
+++ lib/src/fetcher/fetcher.dart
@@ -24,7 +24,14 @@ class Fetcher {
     required Future<DataState<T?>> fetcher,
     required BaseState<T> state,
     required void Function(BaseState<T> state) emitter,
+    void Function(BaseStatus status)? onStatusChange,
   }) async {
+    void onStatusChanged(BaseStatus status) {
+      if (onStatusChange != null) {
+        onStatusChange(status);
+      }
+    }
+    onStatusChanged(BaseStatus.loading);
     BaseState<T> newState = state.copyWith(status: BaseStatus.loading);
     try {
       emitter(newState);
@@ -32,87 +39,91 @@ class Fetcher {
       final result = await fetcher;
 
       if (result is DataSuccess) {
+        onStatusChanged(BaseStatus.success);
         newState = newState.copyWith(data: result.data, status: BaseStatus.success);
         emitter(newState);
       } else if (result is DataFailed) {
+        onStatusChanged(BaseStatus.error);
         newState = newState.copyWith(errorMessage: result.errorMessage, status: BaseStatus.error);
         emitter(newState);
       }
     } catch (e) {
+      onStatusChanged(BaseStatus.error);
       newState = newState.copyWith(errorMessage: e.toString(), status: BaseStatus.error);
       emitter(newState);
     } finally {
+      onStatusChanged(BaseStatus.initial);
       newState = newState.copyWith(status: BaseStatus.initial);
       emitter(newState);
     }
   }
 
-/// fetch with pagination
-/// [fetchWithPaging] is fetching function with base paging state
-///
-/// Params:
-/// [isPaging] is paging status
-/// [size] is size of data
-/// [targetState] is target state, in fact input state
-/// [emitter] is emit, that will use for set,
-/// actually output state
-/// [fetcher] function that used with useCases
-/// [shouldIncrement] is function that used for increment page
-///
-/// Example:
-///
-/// ```dart
-/// await Fetcher.fetchWithPaging(
-/// isPaging: isPaging,
-/// targetState: state.query,
-/// fetcher: _getNotificationHistoryUseCase.call(params: NotificationHistoryQuery(page: state.query.page, size: 3)),
-/// emitter: (newState) {
-/// emit(state.copyWith(query: newState, notificationHistory: newState.query));
-/// },
-/// );
-///  ```
-// static Future<void> fetchWithPaging<T, Q>({
-//   bool isPaging = false,
-//   required BaseQuery<BasePagingState<T, Q>> targetState,
-//   required Future<DataState<List<T>?>> fetcher,
-//   required void Function(BaseQuery<BasePagingState<T, Q>> state) emitter,
-// }) async {
-//   if (!targetState.query.reachedMax || !isPaging) {
-//     targetState = targetState.copyWith(
-//       query: targetState.query.copyWith(
-//         list: isPaging ? targetState.query.list : [],
-//         status: isPaging ? BasePagingStatus.paging : BasePagingStatus.loading,
-//       ),
-//       page: isPaging ? targetState.page : 1,
-//     );
-//     emitter(targetState);
-//
-//     final dataState = await fetcher;
-//     if (dataState is DataSuccess) {
-//       final newList = dataState.data ?? [];
-//       targetState = targetState.copyWith(
-//         query: targetState.query.copyWith(
-//           status: BasePagingStatus.success,
-//           list: isPaging ? [...targetState.query.list, ...newList] : newList,
-//           reachedMax: (dataState.data?.length ?? 0) < targetState.size,
-//         ),
-//         page: targetState.page + 1,
-//       );
-//       emitter(targetState);
-//     } else if (dataState is DataFailed) {
-//       targetState = targetState.copyWith(
-//           query: targetState.query.copyWith(
-//         status: BasePagingStatus.error,
-//         error: dataState.message,
-//       ));
-//       emitter(targetState);
-//     }
-//     targetState = targetState.copyWith(query: targetState.query.copyWith(status: BasePagingStatus.initial));
-//     emitter(targetState);
-//   }
-// }
+  /// fetch with pagination
+  /// [fetchWithPaging] is fetching function with base paging state
+  ///
+  /// Params:
+  /// [isPaging] is paging status
+  /// [size] is size of data
+  /// [targetState] is target state, in fact input state
+  /// [emitter] is emit, that will use for set,
+  /// actually output state
+  /// [fetcher] function that used with useCases
+  /// [shouldIncrement] is function that used for increment page
+  ///
+  /// Example:
+  ///
+  /// ```dart
+  /// await Fetcher.fetchWithPaging(
+  /// isPaging: isPaging,
+  /// targetState: state.query,
+  /// fetcher: _getNotificationHistoryUseCase.call(params: NotificationHistoryQuery(page: state.query.page, size: 3)),
+  /// emitter: (newState) {
+  /// emit(state.copyWith(query: newState, notificationHistory: newState.query));
+  /// },
+  /// );
+  ///  ```
+  // static Future<void> fetchWithPaging<T, Q>({
+  //   bool isPaging = false,
+  //   required BaseQuery<BasePagingState<T, Q>> targetState,
+  //   required Future<DataState<List<T>?>> fetcher,
+  //   required void Function(BaseQuery<BasePagingState<T, Q>> state) emitter,
+  // }) async {
+  //   if (!targetState.query.reachedMax || !isPaging) {
+  //     targetState = targetState.copyWith(
+  //       query: targetState.query.copyWith(
+  //         list: isPaging ? targetState.query.list : [],
+  //         status: isPaging ? BasePagingStatus.paging : BasePagingStatus.loading,
+  //       ),
+  //       page: isPaging ? targetState.page : 1,
+  //     );
+  //     emitter(targetState);
+  //
+  //     final dataState = await fetcher;
+  //     if (dataState is DataSuccess) {
+  //       final newList = dataState.data ?? [];
+  //       targetState = targetState.copyWith(
+  //         query: targetState.query.copyWith(
+  //           status: BasePagingStatus.success,
+  //           list: isPaging ? [...targetState.query.list, ...newList] : newList,
+  //           reachedMax: (dataState.data?.length ?? 0) < targetState.size,
+  //         ),
+  //         page: targetState.page + 1,
+  //       );
+  //       emitter(targetState);
+  //     } else if (dataState is DataFailed) {
+  //       targetState = targetState.copyWith(
+  //           query: targetState.query.copyWith(
+  //         status: BasePagingStatus.error,
+  //         error: dataState.message,
+  //       ));
+  //       emitter(targetState);
+  //     }
+  //     targetState = targetState.copyWith(query: targetState.query.copyWith(status: BasePagingStatus.initial));
+  //     emitter(targetState);
+  //   }
+  // }
 
-/// fetch with custom
+  /// fetch with custom
 }
 
 // class BaseQuery<Q extends BasePagingState> {
--- test/cubit_base_test.dart
+++ test/cubit_base_test.dart
@@ -1,12 +1,32 @@
+import 'package:cubit_base/cubit_base.dart';
+import 'package:cubit_base/src/base_state/base_state.dart';
+import 'package:cubit_base/src/base_state/data_state.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:cubit_base/cubit_base.dart';
-
 void main() {
+  Future<DataState<List<int>>> fetcher(bool error) async {
+    await Future.delayed(Duration(seconds: 2));
+    if (error) return DataFailed(errorMessage: 'error');
+    return DataSuccess(data: [1, 2, 3, 4]);
+  }
+
   test('adds one to input values', () {
-    final calculator = Calculator();
-    expect(calculator.addOne(2), 3);
-    expect(calculator.addOne(-7), -6);
-    expect(calculator.addOne(0), 1);
+    BaseState<List<int>> state = BaseState<List<int>>(data: []);
+    void emitter(BaseState<List<int>> state) {
+
+    }
+    Fetcher.fetchWithBase(
+      fetcher: fetcher(false),
+      state: state,
+      emitter: emitter,
+      onStatusChange: (status) {
+        if (status == BaseStatus.loading) {
+          expect(status, BaseStatus.loading);
+        } else if (status == BaseStatus.success) {
+          expect(status, BaseStatus.success);
+          expect(state.data, [1, 2, 3,4]);
+        }
+      },
+    );
   });
 }
